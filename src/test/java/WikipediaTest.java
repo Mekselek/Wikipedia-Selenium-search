df67a5c613b134385e2097fa8bf59cd798346e68
Mekselek <maksymilian.kwasek@gmail.com>
2023-11-05 22:05:23 +0100

    Added Wiki and Google actions

--- src/test/java/WikipediaTest.java
+++ src/test/java/WikipediaTest.java
@@ -1,6 +1,14 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class WikipediaTest {
     @Test
@@ -9,7 +17,22 @@ public class WikipediaTest {
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://www.google.com");
         driver.manage().window().maximize();
+        WebElement acceptCookies = driver.findElement(By.xpath("//div[text()='Zaakceptuj wszystko']"));
+        acceptCookies.click();
+        WebElement searchField = driver.findElement(By.name("q"));
 
+        new Actions(driver)
+                .sendKeys(searchField, "Wikipedia")
+                .sendKeys(searchField, Keys.ENTER)
+                .perform();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div/span[text()='Wikipedia']")));
+        driver.findElement(By.xpath("//div/span[text()='Wikipedia']")).click();
+        WebElement wikiSearchField = driver.findElement(By.name("search"));
+        new Actions(driver)
+                .sendKeys(wikiSearchField, "Kasztanowiec")
+                .sendKeys(wikiSearchField, Keys.ENTER)
+                .perform();
     }
 
 }
